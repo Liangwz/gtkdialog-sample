0581aa744fe04e46a9c746159de75bf514791ebf
Liangwz <lianweizhang@126.com>
2014-09-28 17:48:53 +0800

    add menubar imagebut wiget

--- shellGtk-TcOS-demo.sh
+++ shellGtk-TcOS-demo.sh
@@ -5,14 +5,15 @@
 #		3 show the imformation about cpu and os.
 #09.18		1 using eval to deal with variable instead of button action
 #09.19		1 add widget checkbox
+#0928		1 add widget imagebut & menubar
 
 #mouse change: right==>xmodmap -e "pointer = 3 2 1"   left==>xmodmap -e "pointer = 1 2 3"
 GTKDIALOG=gtkdialog 
 
 export MOUSE_LEFT="xmodmap -e \"pointer = 1 2 3\""
 export MOUSE_RIGHT="xmodmap -e \"pointer = 3 2 1\""
-#export CPUVAL=`cat /proc/cpuinfo |grep "model name"|cut -f2 -d:`
-export CPUVAL="intell E5500"
+export CPUVAL=`cat /proc/cpuinfo |grep "model name"|cut -f2 -d:`
+#export CPUVAL="intell E5500"
 export OS=`uname -s`
 export _GTKDIALOG="gtkdialog"
 #export _WIFI_NAME=`iwconfig|grep -m 1 'IEEE 802.11'|cut -d ' ' -f 1`
@@ -22,13 +23,13 @@ export _WIFI_ENCRYPTION="none"
 export _WIFI_ESSID="GUEST"
 export _WIFI_KEY="password"
 
-
+IMAGEFILE="`find /usr/share/pixmaps -maxdepth 1 -type f | head -1`"
 
 export _CONF_MOUSE_TYPE="Left"
 export _CONF_HOSTNAME="TC OS"
 export _CONF_LANGUAGE_LOCALE="USA"
 export _CONF_TIME_ZONE="USA"
-export _CONF_UTC_IF_ENABLE="enable"
+export _CONF_UTC_IF_ENABLE="noset"
 export _CONF_KEYBOARD_LAYOUT="USA"
 export _CONF_NUMLOCK_IF_ENABLE="enable"
 
@@ -55,7 +56,37 @@ funcgetTime(){
 }; export -f funcgetTime
 
 
+funcimagebtnCreate() {
+	echo '<button image-position="'$1'" homogeneous="'$2'">
+					<label>'$3'</label>
+					<input file stock="'$3'"></input>
+					<action>""</action>
+				</button>'
+}
 
+funcmenuCreate() {
+	echo '<vbox spacing="0">
+		<menubar>
+			<menu use-underline="true">
+				<menuitem stock-id="gtk-quit" accel-key="0x51" accel-mods="4">
+					<action>exit:Quit</action>
+				</menuitem>
+				<label>"_File"</label>
+			</menu>
+			<menu label="_Other" use-underline="true">
+				<menuitem stock-id="gtk-home" label="Stock Icon"></menuitem>
+				<menuitemseparator></menuitemseparator>
+				<menu image-name="'"$IMAGEFILE"'" label="Image from File">
+					<menu icon-name="gimp" label="Theme Icon">
+						<menuitem label="Label Only"></menuitem>
+					</menu>
+					<height>16</height>
+					<width>16</width>
+				</menu>
+			</menu>
+		</menubar>
+	</vbox>'
+}
 
 
 export VIEW_MESSAGE_DIALOG='
@@ -76,12 +107,17 @@ export MAIN_DIALOG='
 
 <vbox> 
 	<hbox>
+		<vbox>
+			'"`funcmenuCreate`"'
+		</vbox>		
 		<frame>
+
 			<hbox> 
-				<text><label> Detail: </label></text>
 				<text><label> '"${CPUVAL}"'</label></text>
-				<text><label> OS:'"${OS}"' </label></text>
 			</hbox> 
+			<hbox> 
+				<text><label> OS:'"${OS}"' </label></text>
+			</hbox> 			
 		
 			<hbox>
 				<text><label> Mouse </label></text>
@@ -147,7 +183,12 @@ export MAIN_DIALOG='
 					<item>CHINA</item>
 				</comboboxtext>
 			</hbox>
-		
+			<hbox>
+			<checkbox>
+				<label>NumLock</label>
+				<variable>_CONF_NUMLOCK_IF_ENABLE</variable>
+				</checkbox>
+				</hbox>
 			<hbox>
 				<text><label> NumLock </label></text>
 				<combobox>
@@ -176,6 +217,9 @@ export MAIN_DIALOG='
 		
 			<hseparator width-request="240"></hseparator> 
 		
+			<hbox>
+				'"`funcimagebtnCreate 0 true gtk-print`"'
+			</hbox>
 			<hbox>
 				<timer interval="1">
 				'"`functimeShow`"'	
@@ -209,7 +253,7 @@ echo ${_CONF_HOSTNAME}
 echo ${_CONF_MOUSE_TYPE}
 echo ${_CONF_LANGUAGE_LOCALE}
 echo ${_CONF_TIME_ZONE}
-echo ${_CONF_UTC_IF_ENABLE}
+echo CONF_UTC ${_CONF_UTC_IF_ENABLE}
 echo ${_CONF_KEYBOARD_LAYOUT}
 echo ${_CONF_NUMLOCK_IF_ENABLE}
 
